4e2f858628e9ad6ba03b6505df43d628d264e71b
ectos0130 <ectos0130@naver.com>
2023-11-09 16:15:47 +0900

    06

--- main.c
+++ main.c
@@ -3,18 +3,17 @@
 #include <string.h>
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 int main(int argc, char *argv[]) {
-	char str[100];
-	FILE*fp;
-	int i;
+	FILE*fp = NULL;
+	char c;
 	
-	fp = fopen("sample.txt","w");
-	for(i=0;i<3;i++){
-		printf("input a word: ");
-		scanf("%s", str);
-	
-		fprintf(fp, "%s\n", str);
+	fp = fopen("sample.txt", "r");
+	if (fp == NULL){
+		printf("failed to open\n");
+		return 0;
+	}
+	while((c = fgetc(fp)) != EOF){
+		putchar(c);
 	}
 	fclose(fp);
-	
 	return 0;
 }
